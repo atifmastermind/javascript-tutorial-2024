b8a2c6bcf7eaa0548ee7f328d7b747c8edb611d2
Muhammad Atif <Atifmastermind12@gmail.com>
2024-05-09 20:00:15 +0500

    day one

---
+++ js-variables-and-data-types.js
@@ -0,0 +1,41 @@
+// console
+// console.log("Hello");
+
+// ! variables
+
+/*
+ variables are container for data
+name = "Muhammad Atif"; // string
+age = "age"; // ineger
+price = "100.50"; // dacimal
+isFolow = false; // bolen
+x = null; // that means there issn't any value "empty"
+y = undefined; // that means not spacific yet
+console.log(name, age, price);
+
+but we have to used let var or const before variable names
+
+let name = "Muhammad Atif";
+
+
+after ES6 we don't used var now in javascript
+
+var: variable can be re-declared & updated. a global scope variable.
+let: variable cannot be re-declared but can be updated. a block scope variable.
+const: variable can not be re-declared or updated. a block scope variable.
+
+
+let and const are block element
+
+*/
+
+// ! Data types
+/*
+
+Number, String, Boolean, Undefined, Null, BigInt, Symbol 
+
+we can check the data types using typeof keyword
+*/
+
+let age = 30;
+console.log(typeof age);

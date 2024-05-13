9867bcbe415d04f02b9cbff4f9ee350bce7bdf7c
Muhammad Atif <Atifmastermind12@gmail.com>
2024-05-13 15:22:42 +0500

    day two

--- js-variables-and-data-types.js
+++ js-variables-and-data-types.js
@@ -30,12 +30,123 @@ let and const are block element
 */
 
 // ! Data types
+// ? Primitive
 /*
-
+we have 7 primitive data types
 Number, String, Boolean, Undefined, Null, BigInt, Symbol 
-
 we can check the data types using typeof keyword
-*/
-
 let age = 30;
 console.log(typeof age);
+*/
+
+// ? Non-Primitive
+/*
+objects
+
+its is a collections of values 
+
+arrays,functions (object sub category)
+
+const student = {
+  name: "Muhammad Atif",
+  age: 29,
+  cgpa: 8.2,
+  isPass: true,
+};
+
+the way to get value from object //
+console.log(student["name"]);
+console.log(student.isPass);
+
+ assign a new value in object //
+student["lastName"] = "Muhammad Ajmal";
+
+update a value from object //
+student["age"] = student["age"] + 1;
+
+we can update object key value even if object variable is const //
+
+*/
+// ! Operators and condinoal statments
+
+// ? Operators
+
+/*
+
+used to perform some operation on data 
+
+*/
+
+// ? Arithmetic operators
+
+// let a = 5;
+// let b = 2;
+// console.log("a = ", a, "b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+
+// ? Modulus
+
+// console.log("a % b = ", a % b);
+
+// ?Exponentiation
+
+// console.log("a ** b = ", a ** b);
+
+//these operators works on 2 operands (means its required 2 values to give result)
+
+// ? Unary operators
+
+// ? Increment
+// sign++ (means a + 1)
+
+// let a = 5;
+// let b = 2;
+
+// a = a + 1; // 1st way
+
+// a++; // short way
+
+//console.log(a);
+
+// ? Decrement
+
+//sign-- (means a - 1)
+
+// let a = 5;
+// let b = 2;
+
+// a = a - 1; // 1st way
+
+// a--; // short way
+
+// console.log(a);
+
+// ? post Increment
+// a++ (bad main)
+
+// let a = 5;
+// console.log("a++", a++);
+
+// ? pre Increment
+// ++a (Phlay)
+// let a = 5;
+// console.log("++a", ++a);
+
+// ? assignment operators
+
+// in javascript values gose from right side to left side.
+
+// let a = 5;
+// let b = 2;
+// a += 4; // a = a + 4
+// a -= 4; // a = a - 4
+// a *= 4; // a = a * 4
+// a /= 4; // a = a / 4
+// a %= 4; // a = a % 4
+// a **= 4; // a = a ** 4
+// console.log("a =", a);
+
+// ? comparison operators
